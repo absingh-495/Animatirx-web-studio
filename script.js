6af10562f51f4db45ef8c8ebae1f49984796dd07
Abhishek Singh <abhishek.skd13@gmail.com>
2024-05-31 15:21:48 +0530

    final  commit loading section

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ $(document).ready(function(){
   var $scramble = $(".scramble");
   $scramble.scramble(3000, 20,"alphabet",true)
 });
+
+var tl = gsap.timeline();
+tl.to(".loading",{
+  opacity:0,
+  delay:2.5,
+})
+
+// this is for preventing bottom to animation.
+
+.to(".loader",{
+  opacity:0,
+  duration:1,
+  ease:"expo.easeInout",
+})
+
+.to(".loader",{
+  y:"-100%",
+  duration:1,
+})
