a0853e504b52907d85f98464a1a5e8da999acefd
Abhishek Singh <abhishek.skd13@gmail.com>
2024-06-01 21:41:57 +0530

    updated

--- script.js
+++ script.js
@@ -1,23 +1,179 @@
-$(document).ready(function(){
+ // ----------Scramble------------
+$(document).ready(function () {
+  // ELEMENTS
   var $scramble = $(".scramble");
-  $scramble.scramble(3000, 20,"alphabet",true)
+
+  $scramble.scramble(3000, 20, "alphabet", true);
 });
 
+// ----------scrambleEnd--------------
+
 var tl = gsap.timeline();
-tl.to(".loading",{
-  opacity:0,
-  delay:2.5,
+tl.to(".Loading", {
+  opacity: 0,
+  delay: 2.5,
 })
+  .to(".loader", {
+    // y: "-100%",
+    opacity: 0,
+    duration: 1,
+    ease: "Expo.easeInOut",
+  })
+  .to(
+    ".loader",
+    {
+      y: "-100%",
+      // opacity:0,
+      duration: 1,
+      // ease: "Expo.easeInOut"
+    },
+    "up"
+  )
+  .to(
+    ".svgwaala",
+    {
+      opacity: 1,
+      delay: -1.7,
+      ease: "Expo.easeInOut",
+    },
+    "donut"
+  )
+  .to(
+    "#dotted",
+    {
+      opacity: 1,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+    },
+    "donut"
+  )
+  .to(
+    "#donut",
+    {
+      opacity: 1,
+      scale: 2,
+      delay: -1.4,
+      ease: "Expo.easeInOut",
+      rotationY: "+=15",
+      duration: 3,
+      rotationX: "+=15",
+      rotationZ: "+=15",
+    },
+    "up"
+  )
 
-// this is for preventing bottom to animation.
+  .from(
+    "#kothik-k, #kothik-o, #kothik-t, #kothik-h, #kothik-k2, #kothik-i",
+    {
+      opacity: 0,
+      duration: 1,
+      scale: 0,
+      top: "40%",
+      left: "47%",
+    },
+    "up"
+  );
 
-.to(".loader",{
-  opacity:0,
-  duration:1,
-  ease:"expo.easeInout",
-})
+var tl2 = gsap.timeline();
 
-.to(".loader",{
-  y:"-100%",
-  duration:1,
-})
+document.querySelectorAll("#explore").forEach(function (elem) {
+  elem.addEventListener("click", function () {
+    tl2
+      .to(
+        "#kothik-k",
+        {
+          left: "-15%",
+          top: "-15%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#kothik-o",
+        {
+          top: "-20%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#kothik-t",
+        {
+          left: "115%",
+          top: "-20%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#kothik-h",
+        {
+          left: "115%",
+          top: "110%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#kothik-i",
+        {
+          left: "35%",
+          top: "120%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(
+        "#kothik-k2",
+        {
+          left: "-15%",
+          top: "80%",
+          scale: "2",
+          rotate: "25deg",
+          ease: "ease.out",
+          duration: 1,
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to("#donut", {
+        width: "80vw",
+        top: "-10%",
+        rotate: "360deg",
+        ease: "ease.out",
+        duration: 1,
+        opacity: 0,
+        delay: "-1",
+      })
+      .to(
+        "#dotted",
+        {
+          opacity: 0,
+        },
+        "sw"
+      )
+      .to(".svgwaala", {
+        opacity: 0,
+      });
+  });
+});
